3d32136cda9d8c06a744d70c0960388f97389daa
Qasim Hussain <q.hussain@greenstone-energy.de>
2021-02-12 12:19:58 +0100

    added app logic and tee reqs

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
+FROM nickgryg/alpine-pandas:3.9
+
 FROM sconecuratedimages/public-apps:python-3.7.3-alpine3.10-scone3.0
 
 ### install python3 dependencies you need
-RUN SCONE_MODE=sim pip3 install pyfiglet
-
-FROM nickgryg/alpine-pandas:3.9
+#RUN SCONE_MODE=sim pip3 install pyfiglet
 
 #ADD requirements.txt /
 #RUN pip3 install -r /requirements.txt
 
 COPY ./src /app
-COPY test_patient_data.csv /test_patient_data.csv
 
 ###  protect file system with Scone
-#COPY ./protect-fs.sh ./Dockerfile /build/
-#RUN sh /build/protect-fs.sh /app
+COPY ./protect-fs.sh ./Dockerfile /build/
+RUN sh /build/protect-fs.sh /app
 
 ENTRYPOINT ["python", "/app/app.py"]
--- src/app.py
+++ src/app.py
@@ -1,7 +1,22 @@
+import os
+import sys
+import json
 import pandas as pd
+# from pyfiglet import Figlet
+
+iexec_out = os.environ['IEXEC_OUT']
+iexec_in = os.environ['IEXEC_IN']
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+text = ""
+result_label = "Patient: Fast Progressor :{f_progressor}, UMN Problems :{umn_porblems}"
 
+# print("------------")
+# print(iexec_out)
+# print(iexec_in)
+# print(os.path.exists(iexec_in + '/test_patient_data.csv'))
+# print(os.listdir(iexec_in))
 
 # this function takes a dataframe of a patient and returns whether she is a slow progressor or a fast progressor
 def is_fast_progressor(df):
@@ -29,9 +44,20 @@ def has_UMN_problems(df):
     print("Has no UMN Problems")
     return False
 
+#APP Logic
+# Use some confidential assets
+if os.path.exists(iexec_in + '/test_patient_data.csv'):
 
-print("start pulling data")
+    print("start pulling data")
+    patient = pd.read_csv(iexec_in + "/test_patient_data.csv")
+    is_fast_progressor(patient)
+    has_UMN_problems(patient)
+    text = result_label.format(f_progressor = is_fast_progressor(patient), umn_porblems = has_UMN_problems(patient))
 
-patient = pd.read_csv("test_patient_data.csv")
-is_fast_progressor(patient)
-has_UMN_problems(patient)
+# Append some results
+with open(iexec_out + '/result.txt', 'w+') as f:
+    f.write(text)
+
+# Declare everything is computed
+with open(iexec_out + '/computed.json', 'w+') as f:
+    json.dump({ "deterministic-output-path" : iexec_out + '/result.txt' }, f)
